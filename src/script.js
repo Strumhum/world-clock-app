a0da527a4eea1728e7264100137cb88bdb87b705
Strumhum <sue@strumhumcreatives.com>
2023-07-15 13:35:02 +0100

    Added JS for city dropdown selector

--- src/script.js
+++ src/script.js
@@ -1,48 +1,78 @@
 function updateTime() {
   // Madrid
   let madridElement = document.querySelector("#city-1");
-  let madridDateElement = madridElement.querySelector(".date");
-  let madridTimeElement = madridElement.querySelector(".time");
-  let madridTime = moment().tz("Europe/Madrid");
+  if (madridElement) {
+    let madridDateElement = madridElement.querySelector(".date");
+    let madridTimeElement = madridElement.querySelector(".time");
+    let madridTime = moment().tz("Europe/Madrid");
 
-  madridDateElement.innerHTML = madridTime.format("Do MMMM YYYY");
-  madridTimeElement.innerHTML = madridTime.format(
-    `h:mm:ss [<span class="meridian">]A[</span>]`
-  );
+    madridDateElement.innerHTML = madridTime.format("Do MMMM YYYY");
+    madridTimeElement.innerHTML = madridTime.format(
+      `h:mm:ss [<span class="meridian">]A[</span>]`
+    );
+  }
 
   // New York
   let newYorkElement = document.querySelector("#city-2");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
 
-  newYorkDateElement.innerHTML = newYorkTime.format("Do MMMM YYYY");
-  newYorkTimeElement.innerHTML = newYorkTime.format(
-    `h:mm:ss [<span class="meridian">]A[</span>]`
-  );
+    newYorkDateElement.innerHTML = newYorkTime.format("Do MMMM YYYY");
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      `h:mm:ss [<span class="meridian">]A[</span>]`
+    );
+  }
 
   // Sydney
   let sydneyElement = document.querySelector("#city-3");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Australia/Sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
 
-  sydneyDateElement.innerHTML = sydneyTime.format("Do MMMM YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    `h:mm:ss [<span class="meridian">]A[</span>]`
-  );
+    sydneyDateElement.innerHTML = sydneyTime.format("Do MMMM YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      `h:mm:ss [<span class="meridian">]A[</span>]`
+    );
+  }
 
   // Istanbul
   let istanbulElement = document.querySelector("#city-4");
-  let istanbulDateElement = istanbulElement.querySelector(".date");
-  let istanbulTimeElement = istanbulElement.querySelector(".time");
-  let istanbulTime = moment().tz("Asia/Istanbul");
+  if (istanbulElement) {
+    let istanbulDateElement = istanbulElement.querySelector(".date");
+    let istanbulTimeElement = istanbulElement.querySelector(".time");
+    let istanbulTime = moment().tz("Asia/Istanbul");
 
-  istanbulDateElement.innerHTML = istanbulTime.format("Do MMMM YYYY");
-  istanbulTimeElement.innerHTML = istanbulTime.format(
-    `h:mm:ss [<span class="meridian">]A[</span>]`
-  );
+    istanbulDateElement.innerHTML = istanbulTime.format("Do MMMM YYYY");
+    istanbulTimeElement.innerHTML = istanbulTime.format(
+      `h:mm:ss [<span class="meridian">]A[</span>]`
+    );
+  }
+}
+
+function updateCities(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h3>${cityName}</h3>
+            <h4 class="date">${cityTime.format("Do MMMM YYYY")}</h4>
+          </div>
+          <div>
+            <h2 class="time">${cityTime.format(
+              "h:mm:ss"
+            )} <span class="meridian">${cityTime.format("A")}</span></h2>
+          </div>
+        </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city-select");
+citiesSelectElement.addEventListener("change", updateCities);
